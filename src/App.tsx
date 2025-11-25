0a84d7c958dfcc309de44a319557549d4472350f
Subaga <subaga98@gmail.com>
2025-11-25 18:25:43 +0100

    Make api call

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
 import './App.css';
+import { useState } from 'react';
+import axios from 'axios';
+
+interface AdviceData {
+  slip: {
+    id: number;
+    advice: string;
+  };
+}
 
 function App() {
+  const [advice, setAdvice] = useState<AdviceData | null>(null);
+
+  async function loadAdvice() {
+    const res = await axios.get<AdviceData>(
+      'https://api.adviceslip.com/advice'
+    );
+    setAdvice(res.data);
+  }
+  // useEffect(() => {
+  //   loadAdvice();
+  // }, []);
+
   return (
     <div className="adviceContainer">
-      <p className="adviceTitle">Advice # </p>
       <img
         src="/src/assets/images/pattern-divider-desktop.svg"
         alt="desktop-divider"
@@ -14,13 +34,23 @@ function App() {
         alt="mobile-divider"
         className="mobileDivider"
       />
-      <button className='diceBtn'>
+      <button className="diceBtn" onClick={loadAdvice}>
         <img
           src="/src/assets/images/icon-dice.svg"
           alt="dice-icon"
           className="diceIcon"
         />
       </button>
+      {advice ? (
+        <>
+          <p className="adviceTitle">Advice #{advice.slip.id}</p>
+          <p className="adviceText">"{advice.slip.advice}"</p>
+        </>
+      ) : (
+        <p className="loadText">
+          Click below to get an <span>Advice</span>
+        </p>
+      )}
     </div>
   );
 }
